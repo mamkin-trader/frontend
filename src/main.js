eb43eaa256ac0a650e61604e0880ad9b55a5c4cd
Artem Biryukov <impowski@protonmail.ch>
2018-05-27 21:52:47 +0500

    Added Vue PWA template with Apollo-Client and Element UI

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import './registerServiceWorker'
+import { createProvider } from './vue-apollo'
+import './vue-element-ui'
+
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  provide: createProvider().provide(),
+  render: h => h(App)
+}).$mount('#app')
